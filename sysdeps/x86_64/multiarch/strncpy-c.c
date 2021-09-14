eaaa51af732227d95285524c8813f7e64c8377a1
Alexey Kodanev <aleksei.kodanev@bell-sw.com>
2021-09-14 15:36:02 +0300

    move strncpy/stpncpy c impl to string dir

--- sysdeps/x86_64/multiarch/strncpy-c.c
+++ sysdeps/x86_64/multiarch/strncpy-c.c
@@ -1,32 +1,5 @@
-/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <https://www.gnu.org/licenses/>.  */
-
-#include <string.h>
-
 #define STRNCPY __strncpy_sse2
 #undef libc_hidden_builtin_def
 #define libc_hidden_builtin_def(strncpy)
 
-char *
-STRNCPY (char *s1, const char *s2, size_t n)
-{
-  size_t size = strnlen (s2, n);
-  if (size != n)
-    memset (s1 + size, '\0', n - size);
-  return memcpy (s1, s2, size);
-}
-
+#include <string/strncpy.c>
